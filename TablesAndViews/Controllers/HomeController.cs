ab497d6009a6ef58e62d63e9b22ee4494a1193a3
Gabriela Istrate <gistrate09@gmail.com>
2021-05-31 00:30:13 +0200

    Added files via upload

---
+++ TablesAndViews/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TablesAndViews.Models;
+
+namespace TablesAndViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PeopleTable()
+        {
+            PeopleModel.GenerateResultList();
+            return View();
+        }
+
+        public IActionResult RegisteredPeople()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterPeople(string name,string phone,string city)
+        {
+            PeopleModel.AddPerson(name,phone,city);
+            return View("PeopleTable");
+        }
+
+        public IActionResult RemovePerson(int index)
+        {
+            PeopleModel.RemovePerson(index);
+            return View("PeopleTable");
+        }
+
+        [HttpPost]
+        public IActionResult FilterPeople(string filter)
+        {       
+            PeopleModel.FilterBy(filter);
+            return View("PeopleTable");
+        }
+
+        [HttpPost]
+        public IActionResult ResetFilter(string filterReset)
+        {
+            if(filterReset == "reset")
+                PeopleModel.ResetFilter();
+            return View("PeopleTable");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TablesAndViews/Models/PeopleModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablesAndViews.Models
+{
+    public class PeopleModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+
+        public PeopleModel(string name, string phone, string city)
+        {
+            this.Name = name;
+            this.Phone = phone;
+            this.City = city;
+        }
+
+        public static List<PeopleModel> peopleList = new List<PeopleModel>() {
+            new PeopleModel("Ella Persson","077646464","Stockholm"),
+            new PeopleModel("Freddy Johansson","077858585","Gothenburg"),
+            new PeopleModel("Maria Karlsson","077232323","Lund"),
+            new PeopleModel("John Karlsson","077232323","Lund"),            
+            new PeopleModel("Karl Olsson","077232323","Gothenburg"),
+            new PeopleModel("Emma Johansson","077232323","Lund"),
+            new PeopleModel("Karen Olsson","077232323","Lund"),
+            new PeopleModel("Oliver Persson","077232323","Lund"),
+            new PeopleModel("Astrid Olsson","077232323","Gothenburg"),               
+            new PeopleModel("Adam Olsson","077232323","Lund"),
+            new PeopleModel("Lucia Nilsson","077232323","Lund"),
+            new PeopleModel("Erik Persson","077232323","Gothenburg")
+
+        };
+
+        public static List<PeopleModel> peopleListResult = new List<PeopleModel>();
+
+
+        public static void GenerateResultList()
+        {
+            foreach(var person in peopleList)
+            
+                peopleListResult.Add(new PeopleModel(person.Name, person.Phone, person.City));
+            
+        }
+
+        public static void AddPerson(string name, string phone, string city)
+        {
+            peopleList.Add(new PeopleModel(name, phone, city));
+            peopleListResult.Add(new PeopleModel(name, phone, city));
+        }
+
+        public static void RemovePerson(int i)
+        {
+            peopleList.RemoveAt(i);
+            peopleListResult.RemoveAt(i);
+        }
+
+        public static void ResetFilter()
+        {
+            peopleListResult.Clear();
+            GenerateResultList();
+        }
+
+        public static void FilterBy(string filter)
+        {
+            peopleListResult.Clear();
+            foreach(var person in peopleList)
+            {
+                if (person.Name.Contains(filter) || person.City.Contains(filter))
+                {
+                    peopleListResult.Add(new PeopleModel(person.Name, person.Phone, person.City));
+
+                }
+            }
+            
+        }
+
+
+    }
+}
